05a97470528dc65c07b154b189891bb2a1b8b0b3
Yael1SAY <yael.system10@gmail.com>
2020-12-30 14:03:12 -0600

    Se implemanta la validacion de los Beans

--- src/main/java/com/kernel/spring/controller/DireccionController.java
+++ src/main/java/com/kernel/spring/controller/DireccionController.java
@@ -1,18 +1,22 @@
 package com.kernel.spring.controller;
 
-import com.kernel.spring.dto.ClienteDTO;
 import com.kernel.spring.dto.ClienteDirDTO;
 import com.kernel.spring.dto.ClienteFullDTO;
 import com.kernel.spring.model.Direccion;
+import com.kernel.spring.service.ClienteService;
 import com.kernel.spring.service.DireccionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("direccion")
 public class DireccionController {
+    private static final Logger LOG = Logger.getLogger(ClienteService.class.getName());
 
     @Autowired
     DireccionService direccionService;
@@ -29,13 +33,14 @@ public class DireccionController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    Direccion RegistrarDireccion(@RequestBody Direccion direccion){
-        return direccionService.RegistrarDireccion(direccion);
+    Direccion RegistrarDireccion(@Valid @RequestBody Direccion direccion, BindingResult binding){
+        LOG.info("Binding Controller: " + binding);
+        return direccionService.RegistrarDireccion(direccion, binding);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    Direccion ActualizarDireccion(@RequestBody Direccion direccion){
-        return direccionService.ActualizarDireccion(direccion);
+    Direccion ActualizarDireccion(@Valid @RequestBody Direccion direccion, BindingResult binding){
+        return direccionService.ActualizarDireccion(direccion, binding);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
--- src/main/java/com/kernel/spring/service/DireccionService.java
+++ src/main/java/com/kernel/spring/service/DireccionService.java
@@ -7,10 +7,13 @@ import com.kernel.spring.model.Cliente;
 import com.kernel.spring.model.Direccion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.*;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 
 @Service
@@ -21,35 +24,71 @@ public class DireccionService {
     @Autowired
     IDireccionDAO IDireccionDao;
 
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+
     public List<ClienteDirDTO> ObtenerDirecciones(){
-        List<ClienteDirDTO> listD = IDireccionDao.ObtenerDirecciones();
-        return listD;
+        return IDireccionDao.ObtenerDirecciones();
     }
 
     public ClienteFullDTO ObtenerDireccionId(long id){
         ClienteFullDTO dto = new ClienteFullDTO();
         Direccion direccion = IDireccionDao.ObtenerDireccionId(id);
-        Cliente cliente = direccion.getCliente();
-        dto.setNombreCompleto(cliente.getNombre() + " " + cliente.getApellido());
-        dto.setDireccion(direccion.getCalle() + ", " + direccion.getNoExterior() + ", " +
-                direccion.getCodPostal() + ", " + direccion.getEstado() + ", Referencia: " +
-                direccion.getReferencia() );
-        return dto;
+        try{
+            Cliente cliente = direccion.getCliente();
+            dto.setNombreCompleto(cliente.getNombre() + " " + cliente.getApellido());
+            dto.setDireccion(direccion.getCalle() + ", " + direccion.getNoExterior() + ", " +
+                    direccion.getCodPostal() + ", " + direccion.getEstado() + ", Referencia: " +
+                    direccion.getReferencia() );
+            LOG.info("Cliente: " + dto);
+            return dto;
+        }catch (Exception e){
+            LOG.warning("No existe cliente con id: " + id);
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public Direccion RegistrarDireccion(Direccion direccion){
-        return IDireccionDao.RegistrarDireccion(direccion);
+    public Direccion RegistrarDireccion(Direccion direccion, BindingResult binding){
+        LOG.info("bindingResult Service: " + binding);//Muestra cuantos errores arojo
+        Set<ConstraintViolation<Direccion>> violations = validator.validate(direccion);
+        if(!binding.hasErrors()){//Si no hay errores inserta la direccion
+            Direccion dir = IDireccionDao.RegistrarDireccion(direccion);
+            LOG.info("Se inserto corectamente el cliente");
+            return dir;
+        }else {//Si hay errores los imprime y retorna un null
+            LOG.warning("Error no se inserto Cliente");
+            for (ConstraintViolation<Direccion> violation : violations) {
+                LOG.warning(violation.getMessage());
+            }
+            return null;
+        }
     }
 
-    public Direccion ActualizarDireccion(@RequestBody Direccion direccion){
-        return IDireccionDao.ActualizarDireccion(direccion);
+    public Direccion ActualizarDireccion(@RequestBody Direccion direccion, BindingResult binding){
+        LOG.info("bindingResult Service: " + binding);
+        Set<ConstraintViolation<Direccion>> violations = validator.validate(direccion);
+        if(!binding.hasErrors()){
+            Direccion dir = IDireccionDao.ActualizarDireccion(direccion);
+            LOG.info("Se actualizo correctamente el cliente");
+            return dir;
+        }else{
+            LOG.info("Error No se Actualizo cliente");
+            for (ConstraintViolation<Direccion> violation : violations) {
+                LOG.warning(violation.getMessage());
+            }
+            return null;
+        }
+
     }
 
     public void EliminarDireccion(@PathVariable long id){
-        //try {
+        try{
             IDireccionDao.EliminarDireccion(id);
-        //}catch (UnexpectedRollbackException e){
-            //e.printStackTrace();
-        //}
+            LOG.info("Se elimino correctamente el cliente con direccion id: " + id);
+        }catch (Exception e) {
+            LOG.warning("No existe el cliente con id: " + id);
+            e.printStackTrace();
+        }
     }
 }
