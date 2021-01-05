e6a9323031a990e5814ad6dc1c7a419e01bf58df
Yael1SAY <yael.system10@gmail.com>
2021-01-05 17:58:59 -0600

    Se agrega Lombock y dos propiedades mas (correo, edad)

--- src/main/java/com/kernel/spring/dto/ClienteDTO.java
+++ src/main/java/com/kernel/spring/dto/ClienteDTO.java
@@ -1,41 +1,19 @@
 package com.kernel.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ClienteDTO implements Serializable{
 
+    private String correo;
     private String nombre;
     private String apellido;
+    private int edad;
 
-    public ClienteDTO() {
-    }
-
-    public ClienteDTO(String nombre, String apellido) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    @Override
-    public String toString() {
-        return "ClienteDTO{" +
-                "nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                '}';
-    }
 }
